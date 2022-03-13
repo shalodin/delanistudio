e42b9287c2b7bc7473efaa4994f6eb85a75b2417
shalodin <shalodin.chepkoech@student.moringaschool.com>
2022-03-14 01:29:41 +0300

    changed paragraph

---
+++ script.js
@@ -0,0 +1,18 @@
+
+ $(document).ready(function(){
+  $("#show1").click(function(){
+    $("#content1").toggle();
+  });
+});
+
+ $(document).ready(function(){
+  $("#show2").click(function(){
+    $("#content2").toggle();
+  });
+});
+
+ $(document).ready(function(){
+  $("#show3").click(function(){
+    $("#content3").toggle();
+  });
+});
